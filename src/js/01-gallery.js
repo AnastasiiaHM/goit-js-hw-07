64b59e17e662e4d5af0c045f32fe47d32eaa0116
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-02-10 21:12:22 +0100

    add gallerys

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const wraperGalleryEl = document.querySelector(".gallery");
+
+function createGaleryItemsElem(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    })
+    .join("");
+}
+
+const galleryItem = createGaleryItemsElem(galleryItems);
+
+wraperGalleryEl.insertAdjacentHTML("beforeend", galleryItem);
+
+wraperGalleryEl.addEventListener("click", hanleClickElGallery);
+
+window.addEventListener("keydown", handleEscClose);
+
+let instance = "";
+
+function hanleClickElGallery(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let srcActive = event.target.dataset.source;
+
+  instance = basicLightbox.create(` <img
+      class="gallery__image"
+      src="${srcActive}"
+     width ="1200"
+    />`);
+
+  instance.show();
+}
+
+function handleEscClose(event) {
+  const escape = event.code;
+  if (escape === "Escape") {
+    instance.close();
+    console.log("Escape");
+  }
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const wraperGalleryEl = document.querySelector(".gallery");
+
+function createGaleryItemsElem(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+    })
+    .join("");
+}
+
+const galleryItem = createGaleryItemsElem(galleryItems);
+wraperGalleryEl.insertAdjacentHTML("beforeend", galleryItem);
+let galleryImages = new SimpleLightbox(".gallery a", {
+  overlayOpacity: 0.4,
+  captions: true,
+  captionSelector: "img",
+  captionsData: "title",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  animationSpeed: 250,
+});
+galleryImages.on("error.simplelightbox", function (e) {
+  console.log(e);
+});
